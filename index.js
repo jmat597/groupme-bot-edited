5a1f863720fefb7d49be298d25c12bcc436cd33a
Justin <jmat597@gmail.com>
2016-12-07 12:15:36 -0600

    Seriously wtf did I do last night

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ var util = require('util');
 
 
 // local configuration read from env.
+const BOT_ID = process.env['BOT_ID'];
 const TOKEN = process.env['TOKEN']; // your groupme api token
 const GROUP = process.env['GROUP']; // the room you want to join
 const NAME = process.env['NAME']; // the name of your bot
 const URL = process.env['URL']; // the domain you're serving from, should be accessible by Groupme.
-const CONFIG = {token:TOKEN, group:GROUP, name:NAME, url:URL};
+const CONFIG = {token:TOKEN, group:GROUP, name:NAME, url:URL, bot_id:BOT_ID};
 const PORT = process.env.PORT || 8000;
 
 var mybot = bot(CONFIG);
-
-mybot.on('botRegistered', function(b) {
-  console.log("I am registered");
-  b.message("WHAT UP BRO?");
-});
 
 mybot.on('botMessage', function(b, message) {
   console.log("I got a message, fyi");
@@ -25,7 +21,7 @@ mybot.on('botMessage', function(b, message) {
 });
 
 var now = new Date();
-var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 1, 05, 0, 0) - now;
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0, 0) - now;
 if (millisTill10 < 0) {
      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
 }
